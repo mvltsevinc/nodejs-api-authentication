894ff3bcd29b55fc39dcd02bb8b5f36c5a4fe565
Mevlut <sevincmevlut@gmail.com>
2019-10-26 22:58:30 +0300

    jwt added

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const User = require('../model/User');
+const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 const { registerValidation, loginValidation } = require('../validation');
 
@@ -30,6 +31,25 @@ router.post('/register', async (req, res) => {
   } catch (err) {
     res.status(400).send(err);
   }
+});
+
+//Login user
+router.post('/login', async (req, res) => {
+  //Validate the data before we a user
+  const { error } = loginValidation(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  //Checking if the email exists
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) return res.status(400).send('Email is not found!');
+
+  //Checking the password
+  const validPassword = await bcrypt.compare(req.body.password, user.password);
+  if (!validPassword) return res.status(400).send('Invalid password!');
+
+  //Create and assign a token
+  const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET);
+  res.header('auth-token', token).send(token);
 });
 
 module.exports = router;
